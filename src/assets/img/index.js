9930357ebb90a3f7328338d768a267fa349566ee
smitchaute <60522646+smitchaute@users.noreply.github.com>
2020-12-13 23:03:34 +0530

    Deploying to netlify

---
+++ src/assets/img/index.js
@@ -0,0 +1,8 @@
+import image1 from './a2.jpg'
+import image2 from './g1.jpg'
+
+
+export {
+    image1,
+    image2
+}
---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,47 @@
+import React from "react";
+// react plugin for creating charts
+// @material-ui/core
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import GridItem from "../../components/Grid/GridItem.js";
+import GridContainer from "../../components/Grid/GridContainer.js";
+import Table from "../../components/Table/Table.js";
+import Card from "../../components/Card/Card.js";
+import CardHeader from "../../components/Card/CardHeader.js";
+import CardBody from "../../components/Card/CardBody.js";
+
+import styles from "../../assets/jss/material-dashboard-react/views/dashboardStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Dashboard() {
+  const classes = useStyles();
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="warning">
+              <h4 className={classes.cardTitleWhite}>Feedback</h4>
+              <p className={classes.cardCategoryWhite}>
+                Response from visited Users
+              </p>
+            </CardHeader>
+            <CardBody>
+              <Table
+                tableHeaderColor="warning"
+                tableHead={["ID", "Name", "Message"]}
+                tableData={[
+                  ["1", "Dakota Rice", "Niger"],
+                  ["2", "Minerva Hooper","Curaçao"],
+                  ["3", "Sage Rodriguez", "Netherlands"],
+                  ["4", "Philip Chaney", "Korea, South"]
+                ]}
+              />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
